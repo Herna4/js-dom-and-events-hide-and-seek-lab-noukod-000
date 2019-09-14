d423c10d1ad44729c735a0638e99f101089c9c07
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-14 02:06:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+   const rankedLists = document.querySelectorAll('.ranked-list')
+   
+   for(let i=1, i < rankedLists.length; i++ ){
+     
+   }
+}
+
+ for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+      }
+      
+      for (let i = 0; i < lis.length; i++) {
+      lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+    }
