131b1f3b1768ba871d52dc5a24ca53a9d5a2ed80
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-14 02:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,8 @@ function increaseRankBy(n){
    const rankedLists = document.querySelectorAll('.ranked-list')
    
    for(let i = 0; i < rankedLists.length; i++ ){
-     rankedLists[i].innerHTML = parseInt(rankedLists[i])
+     rankedLists[i].innerHTML = parseInt((rankedLists[i]).innerHTML) + parseInt(m)
    }
 }
 
- for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
-      }
-      
-      for (let i = 0; i < lis.length; i++) {
-      lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
-    }
+ 
